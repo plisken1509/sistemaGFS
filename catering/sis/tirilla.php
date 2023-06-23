52d338a0a4d14b7dd9fc6ac78a3efc00c29a083d
plisken1509 <solorzanopablo81@gmail.com>
2023-06-23 08:37:44 -0500

    tirilla

--- catering/sis/tirilla.php
+++ catering/sis/tirilla.php
@@ -5,13 +5,21 @@ use Mike42\Escpos\EscposImage;
 use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
     include ('conexion.php');
     if (isset($_GET['enviar'])) {
+      $producidos=$_REQUEST['producidos'];
+      $consumos=$_REQUEST['consumos'];
+      $sobrantes=$_REQUEST['sobrantes'];
       $query20="select * from configuracion where nombre='impresora' and descripcion='si'";
       $enviar20=mysqli_query($db,$query20);
       $ver20=mysqli_fetch_array($enviar20);
+      $query50="SELECT DATE(NOW()) AS fecha, CURTIME() AS hora;";
+      $enviar50=mysqli_query($db,$query50);
+      $ver50=mysqli_fetch_array($enviar50);
       $nombre_impresora = $ver20['observacion']; 
+      $fecha= $ver50['fecha']; 
+      $hora= $ver50['hora']; 
                 $connector = new WindowsPrintConnector($nombre_impresora);
                 $printer = new Printer($connector);
-                $printer->text("****Gourmet Food Service****\n****EXTRA SIN ALMUERZO****\nCliente: $nombre\nCedula: $cedula\nEmpresa: $cod\n\n$mensaje****GRACIAS****");
+                $printer->text("        ****Gourmet Food Service****\n         ****Tirilla de Consumos****\n\n            $fecha - $hora   \n\nComedor: Aneffi\n\nProducidos: $producidos\n\nConsumos: $consumos\n\nSobrantes: $sobrantes\n\n\n\n\n              --------------------\n                Firma Responsable\n\nNombre Responsable:\n\n\n\n                ****GRACIAS****");
                 $printer->feed();
                 $printer->cut();
                 $printer->pulse();
@@ -22,6 +30,9 @@ use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
     $query="select * from consumos where fecha=curdate()";
     $enviar=mysqli_query($db,$query);
     $consumos=mysqli_num_rows($enviar);
+    $query5="select * from extras where fecha=curdate() and descripcion='Almuero Extra'";
+    $enviar5=mysqli_query($db,$query5);
+    $consumosextras=mysqli_num_rows($enviar5);
 ?>
 <script type="text/javascript" src="este.js"></script> 
 <script>
@@ -136,7 +147,7 @@ use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 
             <div class="form_div">
                 <label>Consumos:</label>
-                <input class="field_class" name="consumos" id="consumos" type="number" required readonly="" value="<?php echo $consumos ?>">
+                <input class="field_class" name="consumos" id="consumos" type="number" required readonly="" value="<?php echo $consumos+$consumosextras ?>">
                 <label>Producidos:</label>
                 <input class="field_class" name="producidos" id="producidos" type="number" required onchange="sobra()">
                 <label>Sobrantes:</label>
